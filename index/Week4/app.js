c1e46513870bfc04818af9e294a86ab223e92b06
Kumbam Karthik <karthik.april11@gmail.com>
2022-05-13 21:53:35 +0530

    updated

--- index/Week4/app.js
+++ index/Week4/app.js
@@ -1,7 +1,9 @@
 document.getElementById("button").addEventListener("click", submit);
 var clear1 = document.querySelector(".bt1");
 var clear2 = document.querySelector(".bt2");
+var special = document.querySelector("ul");
 var it = document.querySelector(".ct1");
+var cbox = document.querySelector(".pt2");
 it.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
@@ -41,11 +43,41 @@ function Task(value) {
   ul.className = "eachTasks";
   li.className = "eachLi";
   document.querySelector(".tasks").style.display = "block";
-  console.log()
+  console.log();
 }
 clear2.addEventListener("click", DeleteAll);
 function DeleteAll() {
-  ul.innerHTML = '';
+  ul.innerHTML = "";
   btns.children[0].style.display = "none";
   btns.children[1].style.display = "none";
+}
+special.addEventListener("click", deleteItem);
+function deleteItem(event) {
+  if (event.target.className === "material-symbols-outlined") {
+    let remove = event.target.parentNode;
+    let parent = remove.parentNode;
+    parent.removeChild(remove);
+    var len = document.querySelectorAll("li").length;
+    if (len === 0) {
+      btns.children[0].style.display = "none";
+      btns.children[1].style.display = "none";
+    }
+  }
+}
+clear1.addEventListener("click", clearSpecial);
+function clearSpecial(event) {
+  var items = special.getElementsByClassName("pt2");
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].checked) {
+      let remove = items[i].parentNode;
+      let parent = remove.parentNode;
+      parent.removeChild(remove);
+      i--;
+    }
+  }
+  var len = document.querySelectorAll("li").length;
+    if (len === 0) {
+      btns.children[0].style.display = "none";
+      btns.children[1].style.display = "none";
+    }
 }
